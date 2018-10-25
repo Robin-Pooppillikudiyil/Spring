c16269afa2f374f01185d5cdf486b98fcd4bc559
Robin <robinthomasp@gmx.com>
2018-10-25 11:41:18 +0530

    spring-mvc-form-tags-drop-down-lists

---
+++ 130.solution-code-spring-mvc-form-tags-drop-down-lists/src/com/springlearn/springdemo/mvc/HomeController.java
@@ -0,0 +1,13 @@
+package com.springlearn.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String showPage() {
+		return "main-menu";		//1-->goto main-menu.jsp
+	}
+}
