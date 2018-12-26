68487473616f0634bc22356dd140221d1a9bb0d6
Robin <robinthomasp@gmx.com>
2018-12-26 16:37:08 +0530

    csrf protection

---
+++ spring security/438.solution-code-spring-security-demo-06-csrf/src/main/java/com/springlearn/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package com.springlearn.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
